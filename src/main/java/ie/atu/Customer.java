1c9fda3923995e7c2cea2f453cdeb98291627ae8
RIADBOUSSOUFA-STUDEN <g00435390@atu.ie>
2024-11-04 09:24:22 +0000

    added strings

--- src/main/java/ie/atu/Customer.java
+++ src/main/java/ie/atu/Customer.java
@@ -1,4 +1,30 @@
 package ie.atu;
 
 public class Customer {
+    String name;
+    String id;
+    String type;
+    public String getName(){
+        return none;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
